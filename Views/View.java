7f68f62f8cd1af8bc1faa06ede03a24e8593f47b
Ashkan Zeinolabedini <ashkan-oakville@hotmail.com>
2022-11-17 17:58:46 -0500

    started abstract class for the View

--- Views/View.java
+++ Views/View.java
@@ -1,2 +1,13 @@
-package Views;public class View {
+package Views;
+import javafx.stage.Stage;
+import javafx.scene.canvas.GraphicsContext;
+abstract class View {
+    Stage stage;
+
+    public abstract void initUI();
+
+    public abstract void drawScreen();
+
 }
+
+
