2a7529c58fb3a7b1667c52f85da11431dfab7009
Eric So <eric.so.dev@gmail.com>
2022-11-26 03:10:27 -0500

    [DEV-2.2] added interface

--- Model/PromptStatistics.java
+++ Model/PromptStatistics.java
@@ -1,4 +1,114 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class PromptStatistics {
+    private int score;
+    private ArrayList<String> mistypedWords;
+    private ArrayList<String> totalWords;
+    private int correctlyTypedCharacters;
+    private double time;
+    private String phrase;
+
+    /**
+     * Constructor for PromptStatistics
+     */
+    PromptStatistics() {
+        this.phrase = "";
+        time = 0;
+        correctlyTypedCharacters = 0;
+        totalWords = new ArrayList<>();
+        mistypedWords = new ArrayList<>();
+    }
+
+    /**
+     * Constructor for PromptStatistics
+     * @param phrase
+     */
+    PromptStatistics(String phrase) {
+        this();
+        this.phrase = phrase;
+    }
+
+    /**
+     * Adds a character that the user typed
+     * @param c
+     * @param correct
+     */
+    void addCharacter(char c, boolean correct) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Reset the statistics state
+     */
+    void resetStatistics() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Changes the phrase that is currently being typed
+     * @param phrase
+     */
+    void setPhrase(String phrase) {
+        this.phrase = phrase;
+    }
+
+    /**
+     * Records the time taken to type all phrases since the last reset
+     * @param time in seconds
+     */
+    void setTime(double time) {
+        this.time = time;
+    }
+
+    /**
+     * Getter for score
+     * @return score
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * Getter for the user's mistyped words. This list may contain duplicates depending on whether the user mistypes
+     * a word multiple times.
+     * @return mistypedWords
+     */
+    public ArrayList<String> getMistypedWords() {
+        return mistypedWords;
+    }
+
+    /**
+     * Getter for a list of words typed by the user.
+     * @return totalWords
+     */
+    public ArrayList<String> getTotalWords() {
+        return mistypedWords;
+    }
+
+    /**
+     * Getter for the time taken to type what is currently typed in seconds.
+     * @return time
+     */
+    public double getTime() {
+        return time;
+    }
+
+
+    /**
+     * Calculate WPM of what is currently typed
+     * @return WPM
+     */
+    public int calculateWPM() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Calculate the accuracy of what is currently typed
+     * @return accuracy in percentage
+     */
+    public int calculateAccuracy() {
+        throw new UnsupportedOperationException();
+    }
 }
--- Model/TypingStatistics.java
+++ Model/TypingStatistics.java
@@ -1,4 +1,87 @@
 package Model;
 
-public class TypingStatistics {
+import java.util.List;
+
+public class TypingStatistics implements Observable<PromptStatistics> {
+    private List<Observer<PromptStatistics>> observers;
+    private PromptStatistics state;
+
+    /**
+     * Constructor for TypingStatistics
+     */
+    public TypingStatistics() {
+        state = new PromptStatistics();
+    }
+
+    /**
+     * Constructor for TypingStatistics
+     * @param phrase
+     */
+    public TypingStatistics(String phrase) {
+        state = new PromptStatistics(phrase);
+    }
+
+    /**
+     * Adds a character that the user typed
+     * @param c
+     * @param correct
+     */
+    public void addCharacter(char c, boolean correct) {
+        state.addCharacter(c, correct);
+        notifyObservers();
+    }
+
+    /**
+     * Sets the phrase currently being typed
+     * @param phrase
+     */
+    public void changePhrase(String phrase) {
+        state.setPhrase(phrase);
+        notifyObservers();
+    }
+
+    /**
+     * Sets the time taken to type what is currently typed
+     * @param time in seconds
+     */
+    public void setTime(double time) {
+        state.setTime(time);
+        notifyObservers();
+    }
+
+    /**
+     * Resets the state of the statistics model
+     */
+    public void resetStatistics() {
+        state.resetStatistics();
+        notifyObservers();
+    }
+
+    /**
+     * Register a new observer
+     * @param o
+     */
+    @Override
+    public void register(Observer<PromptStatistics> o) {
+        if (!observers.contains(o)) observers.add(o);
+    }
+
+    /**
+     * Unregister an observer
+     * @param o
+     */
+    @Override
+    public void unRegister(Observer<PromptStatistics> o) {
+        observers.remove(o);
+    }
+
+    /**
+     * Notifies all observers of an update in state
+     */
+    @Override
+    public void notifyObservers() {
+        for (Observer<PromptStatistics> o : observers) {
+            o.update(state);
+        }
+    }
 }
