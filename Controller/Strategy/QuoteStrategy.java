6e2a2c172ea57adf8ba885d93cb88fadf095c2f3
Ashkan Zeinolabedini <ashkan-oakville@hotmail.com>
2022-11-30 18:07:07 -0500

    Retrieves quote prompts according to user specification

--- Controller/Strategy/QuoteStrategy.java
+++ Controller/Strategy/QuoteStrategy.java
@@ -1,29 +1,28 @@
 package Controller.Strategy;
 
-import PromptGenerator.PromptGenerator;
 import PromptGenerator.NumberDecorator;
 import PromptGenerator.SymbolDecorator;
+import PromptGenerator.PQuotePromptGenerator;
+import PromptGenerator.NPQuotePromptGenerator;
 
 import java.io.IOException;
 
 public class QuoteStrategy implements Strategy{
-    PromptGenerator promptGenerator;
+    PQuotePromptGenerator pQuotes;
+    NPQuotePromptGenerator nPQuotes;
 
     public QuoteStrategy() throws IOException {
-        this.promptGenerator = new PromptGenerator();
+        pQuotes = new PQuotePromptGenerator();
+        nPQuotes = new NPQuotePromptGenerator();
     }
 
     @Override
     public String execute(int[] data) {
-        if (data[0] == 1 && data[1] == 1) {
-            SymbolDecorator symDec = new SymbolDecorator(promptGenerator);
-            return new NumberDecorator(symDec).getNextPrompt();
-        } else if (data[0] == 1) {
-            return new SymbolDecorator(promptGenerator).getNextPrompt();
-        } else if (data[1] == 1) {
-            return new NumberDecorator(promptGenerator).getNextPrompt();
-        } else {
-            return promptGenerator.getNextPrompt();
+        if (data[0] == 1){
+            return pQuotes.getNextPrompt();
+        }
+        else{
+            return nPQuotes.getNextPrompt();
         }
     }
 }
