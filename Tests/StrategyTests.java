50e88f2d29c6731890790f0fe5e78a6d790ac196
Ashkan Zeinolabedini <ashkan-oakville@hotmail.com>
2022-11-30 15:54:13 -0500

    [DEV 3.5] Wrote Normal Strategy Tests

--- Tests/StrategyTests.java
+++ Tests/StrategyTests.java
@@ -3,6 +3,93 @@ import Controller.Context;
 import Controller.Strategy.*;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
 
 public class StrategyTests {
+
+    @Test
+    void symbolCheck() throws IOException {
+        Context cont = new Context();
+        NormalStrategy norm = new NormalStrategy();
+
+        cont.setStrategy(norm);
+        String[] symbols = {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", ":", ";", "'", ",", ".", "?"};
+
+        String phrase = cont.executeStrategy(new int[] {1,0});
+
+        boolean contains = false;
+        while (!contains){
+            for(String s: symbols){
+                if (phrase.contains(s)){
+                    contains = true;
+                    break;
+                }
+            }
+            phrase = cont.executeStrategy(new int[] {1,0});
+        }
+    }
+
+    @Test
+    void numCheck() throws IOException {
+        Context cont = new Context();
+        NormalStrategy norm = new NormalStrategy();
+
+        cont.setStrategy(norm);
+        String[] symbols = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+        String phrase = cont.executeStrategy(new int[] {0,1});
+
+        boolean contains = false;
+        while (!contains){
+            for(String s: symbols){
+                if (phrase.contains(s)){
+                    contains = true;
+                    break;
+                }
+            }
+            phrase = cont.executeStrategy(new int[] {0,1});
+        }
+    }
+
+    @Test
+    void numSymbolCheck() throws IOException {
+        Context cont = new Context();
+        NormalStrategy norm = new NormalStrategy();
+
+        cont.setStrategy(norm);
+        String[] nums = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        String[] symbols = {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", ":", ";", "'", ",", ".", "?"};
+
+        String phrase = cont.executeStrategy(new int[] {1,1});
+
+        boolean[] contains = {false, false};
+
+        while (!contains[0] && !contains[1]){
+
+
+            for(String s: symbols){
+                if (phrase.contains(s)){
+                    contains[0] = true;
+                    break;
+                }
+            }
+            for (String n: nums){
+                if (phrase.contains(n)){
+                    contains[1] = true;
+                    break;
+
+                }
+            }
+            if(!contains[0] || !contains[1]){
+                contains[0] = false;
+                contains[1] = false;
+            }
+            phrase = cont.executeStrategy(new int[] {1,1});
+        }
+        assertTrue(contains[0]);
+        assertTrue(contains[1]);
+    }
 }
